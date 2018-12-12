5f2d98f7ffa374b278a0d156d72cc078b967e22c
denghuo <973761689@qq.com>
2018-12-12 19:19:48 +0800

    add: 游客登录

--- module/redisKeyMap.js
+++ module/redisKeyMap.js
@@ -4,6 +4,7 @@ module.exports = {
 	QUERY_LOG_ID: "blog:log:ids",  // type: set
 	QUERY_LOG_CONENT: "blog:log:content:",
 	QUERY_LOG_GROUPS: "blog:log:groups",  // type: set
+	QUERY_LOG_COMMENTS : "blog:log:comment:",
 
 	USER_STATIC: "blog:users",
 	QUERY_USER: "blog:user:",
@@ -12,4 +13,6 @@ module.exports = {
 	ALUM_STATIC: "blog:alums",
 	QUERY_ALUM: "blog:alum:",
 	QUERY_ALUM_DESC: "blog:alum:desc:",
+
+	GUEST_MEMBERS : "blog:guests:",
 }
